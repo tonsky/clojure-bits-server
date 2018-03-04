812cc4735d0ce6e07058b10479c29755f0fbea05
Nikita Prokopov <prokopov@gmail.com>
2018-03-04 20:15:59 +0300

    populate fixtures dynamically

---
+++ bits/bits.tonsky.math/floor.cljc
@@ -0,0 +1,4 @@
+(defn floor
+  "The largest long value that is smaller than or equal than the argument"
+  [x]
+  (long (Math/floor (double x))))
--- dev/bits/server/repl.clj
+++ dev/bits/server/repl.clj
@@ -16,61 +16,49 @@
 (set! *warn-on-reflection* true)
 
 
+(bits/require [bits.tonsky.time :as time :just [now]])
+
+
 (defn fixtures! []
-  (println "Fixtures:")
-  (println "  inserting prokopov@gmail.com")
+  (println "Inserting fixtures...")
   (ds/transact! db/*db
-    (concat
-      [{ :db/id              -1
-         :session/id         "ZFcB6peMogWWQ45hq4w5x9xAl0-a4fXE"
-         :session/created    (System/currentTimeMillis)
-         :session/csrf-token (pages.sign-in/new-token)
-         :session/user       -2 }
-       { :db/id              -2
-         :user/display-email "prokopov@gmail.com"
-         :user/display-name  "Nikita Prokopov"
-         :user/email         "prokopov@gmail.com"
-         :user/namespace     "tonsky" }
-       { :db/id              -3
-         :user/display-email "james@booleanknot.com"
-         :user/display-name  "James Reeves"
-         :user/email         "james@booleanknot.com"
-         :user/namespace     "weavejester" }
-       { :db/id              -4
-         :user/display-email "prismatic-plumbing@googlegroups.com"
-         :user/display-name  "Plumatic"
-         :user/email         "prismatic-plumbing@googlegroups.com"
-         :user/namespace     "plumatic" }]
-    (for [[fqn author] [["bits.tonsky.dom/q" -2]
-                        ["bits.tonsky/println*" -2]
-                        ["bits.tonsky.coll/find" -2]
-                        ["bits.tonsky.coll/index-of" -2]
-                        ["bits.tonsky.coll/zip" -2]
-                        ["bits.tonsky.hash/md5" -2]
-                        ["bits.tonsky.math/ceil" -2]
-                        ["bits.tonsky.time/now" -2]
-                        ["bits.tonsky.string/left-pad" -2]
-                        ["bits.tonsky.string/not-blank" -2]
-                        ["bits.weavejester.maps/assoc-some" -3]
-                        ["bits.weavejester.maps/dissoc-in"  -3]
-                        ["bits.weavejester.maps/map-kv"     -3]
-                        ["bits.weavejester/queue?"          -3]
-                        ["bits.weavejester/deref-swap!"     -3]
-                        ["bits.plumatic.plumbing/frequencies-fast" -4]
-                        ["bits.plumatic.plumbing/grouped-map"      -4]
-                        
-                        ["bits.plumatic.plumbing/singleton"        -4]]
-          :let [_ (println "  inserting" fqn)
-                body (bits/fetch fqn)
-                [namespace name] (str/split fqn #"/")
-                parsed (bits/parse-defn-form (bits/read-clojure body))]]
+    [{ :db/id              -1
+       :session/id         "ZFcB6peMogWWQ45hq4w5x9xAl0-a4fXE"
+       :session/created    (time/now)
+       :session/csrf-token (pages.sign-in/new-token)
+       :session/user       -2 }
+     { :db/id              -2
+       :user/display-email "prokopov@gmail.com"
+       :user/display-name  "Nikita Prokopov"
+       :user/email         "prokopov@gmail.com"
+       :user/namespace     "tonsky" }
+     { :user/display-email "james@booleanknot.com"
+       :user/display-name  "James Reeves"
+       :user/email         "james@booleanknot.com"
+       :user/namespace     "weavejester" }
+     { :user/display-email "prismatic-plumbing@googlegroups.com"
+       :user/display-name  "Plumatic"
+       :user/email         "prismatic-plumbing@googlegroups.com"
+       :user/namespace     "plumatic" }])
+  (ds/transact! db/*db
+    (for [^java.io.File file  (file-seq (clojure.java.io/file "bits"))
+          :when (.isFile file)
+          :when (str/ends-with? (.getName file) ".cljc")
+          :let  [path      (str (.getName (.getParentFile file))
+                                "/"
+                                (subs (.getName file) 0 (- (count (.getName file)) (count ".cljc"))))
+                 fqn       (bits/path->fqn path)
+                 body      (slurp file)
+                 [namespace name] (str/split fqn #"/")
+                 user-ns (second (str/split namespace #"\."))
+                 parsed  (bits/parse-defn-form (bits/read-clojure body))]]
       { :bit/fqn       fqn
         :bit/namespace namespace
         :bit/name      name
         :bit/docstring (:docstring parsed)
         :bit/body      body
-        :bit/author    author
-        :bit/created   (.getTime #inst "2018-03-04T16:55:00.000") }))))
+        :bit/author    (:db/id (ds/entity @db/*db [:user/namespace user-ns]))
+        :bit/created   (.getTime #inst "2018-03-04T16:55:00.000") })))
 
 
 (defn -main [& args]
